67a8be8f21a43ea6c55106de363dcfeba8792f5c
petya-stoeva <stoeva@bc.edu>
2025-07-11 15:48:05 -0400

    Changing Internal Links

--- components/RenderNav.js
+++ components/RenderNav.js
@@ -2,10 +2,10 @@ export default function renderNavbar(data) {
   const navbar = document.getElementById("topbar");
   navbar.innerHTML = `
     <ul>
-      <li><a href="https://petyastoeva.glitch.me/">ABOUT</a></li>
-      <li><a href="https://petyastoeva.glitch.me/#skills">SKILLS</a></li>
-      <li><a href="https://petyastoeva.glitch.me/#news">NEWS</a></li>
-      <li><a href="https://petyastoeva.glitch.me/#proj">PROJECTS</a></li>
+      <li><a href="https://petya-stoeva.github.io/petyastoeva/">ABOUT</a></li>
+      <li><a href="https://petya-stoeva.github.io/petyastoeva/#skills">SKILLS</a></li>
+      <li><a href="https://petya-stoeva.github.io/petyastoeva/#news">NEWS</a></li>
+      <li><a href="https://petya-stoeva.github.io/petyastoeva/#proj">PROJECTS</a></li>
       <li><a href="${data.left["my resume"]}">RESUME</a></li>
     </ul>
   `;
